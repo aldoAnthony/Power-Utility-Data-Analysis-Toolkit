c1fae9510888638f1274432d810f735177fadbf2
Aldo Sauls <161869423+aldoAnthony@users.noreply.github.com>
2024-03-21 18:02:17 +0200

    Update word_splitter.py

--- code/word_splitter.py
+++ code/word_splitter.py
@@ -1,97 +1,40 @@
 import pandas as pd
 import numpy as np
 
-""" ### Data Loading and Preprocessing ### """
+""" 
+Data Loading and Preprocessing
+"""
 
-# Electricification by province (EBP) data
-#ebp_url = 'https://raw.githubusercontent.com/Explore-AI/Public-Data/master/Data/electrification_by_province.csv'
-ebp_path = '~/Python_Programs/Explorer_Academy_Course_Work/Predicts/electrification_by_province.csv'
-ebp_df = pd.read_csv(ebp_path)
-#ebp_df = pd.read_csv(ebp_url)
-
-# ~/Python_Programs/Explorer_Academy_Course_Work/Predicts
-for col, row in ebp_df.iloc[:,1:].items():
-    ebp_df[col] = ebp_df[col].str.replace(',','').astype(int)
-
-#print(ebp_df.head())
-ebp_df.head()
-
-# Twitter data
-#twitter_url = 'https://raw.githubusercontent.com/Explore-AI/Public-Data/master/Data/twitter_nov_2019.csv'
-twitter_path = '~/Python_Programs/Explorer_Academy_Course_Work/Predicts/twitter_nov_2019.csv'
+# Twitter data loading
+twitter_path = 'python\data\\twitter_nov_2019.csv'
 twitter_df = pd.read_csv(twitter_path)
-#print(twitter_df.head())
-twitter_df.head()
-
-
-""" ### Important Variables ### """
-# gauteng ebp data as a list
-gauteng = ebp_df['Gauteng'].astype(float).to_list()
-
-# dates for twitter tweets
-dates = twitter_df['Date'].to_list()
-
-# dictionary mapping official municipality twitter handles to the municipality name
-mun_dict = {
-    '@CityofCTAlerts' : 'Cape Town',
-    '@CityPowerJhb' : 'Johannesburg',
-    '@eThekwiniM' : 'eThekwini' ,
-    '@EMMInfo' : 'Ekurhuleni',
-    '@centlecutility' : 'Mangaung',
-    '@NMBmunicipality' : 'Nelson Mandela Bay',
-    '@CityTshwane' : 'Tshwane'
-}
-
-# dictionary of english stopwords
-stop_words_dict = {
-    'stopwords':[
-        'where', 'done', 'if', 'before', 'll', 'very', 'keep', 'something', 'nothing', 'thereupon', 
-        'may', 'why', '’s', 'therefore', 'you', 'with', 'towards', 'make', 'really', 'few', 'former', 
-        'during', 'mine', 'do', 'would', 'of', 'off', 'six', 'yourself', 'becoming', 'through', 
-        'seeming', 'hence', 'us', 'anywhere', 'regarding', 'whole', 'down', 'seem', 'whereas', 'to', 
-        'their', 'various', 'thereafter', '‘d', 'above', 'put', 'sometime', 'moreover', 'whoever', 'although', 
-        'at', 'four', 'each', 'among', 'whatever', 'any', 'anyhow', 'herein', 'become', 'last', 'between', 'still', 
-        'was', 'almost', 'twelve', 'used', 'who', 'go', 'not', 'enough', 'well', '’ve', 'might', 'see', 'whose', 
-        'everywhere', 'yourselves', 'across', 'myself', 'further', 'did', 'then', 'is', 'except', 'up', 'take', 
-        'became', 'however', 'many', 'thence', 'onto', '‘m', 'my', 'own', 'must', 'wherein', 'elsewhere', 'behind', 
-        'becomes', 'alone', 'due', 'being', 'neither', 'a', 'over', 'beside', 'fifteen', 'meanwhile', 'upon', 'next', 
-        'forty', 'what', 'less', 'and', 'please', 'toward', 'about', 'below', 'hereafter', 'whether', 'yet', 'nor', 
-        'against', 'whereupon', 'top', 'first', 'three', 'show', 'per', 'five', 'two', 'ourselves', 'whenever', 
-        'get', 'thereby', 'noone', 'had', 'now', 'everyone', 'everything', 'nowhere', 'ca', 'though', 'least', 
-        'so', 'both', 'otherwise', 'whereby', 'unless', 'somewhere', 'give', 'formerly', '’d', 'under', 
-        'while', 'empty', 'doing', 'besides', 'thus', 'this', 'anyone', 'its', 'after', 'bottom', 'call', 
-        'n’t', 'name', 'even', 'eleven', 'by', 'from', 'when', 'or', 'anyway', 'how', 'the', 'all', 
-        'much', 'another', 'since', 'hundred', 'serious', '‘ve', 'ever', 'out', 'full', 'themselves', 
-        'been', 'in', "'d", 'wherever', 'part', 'someone', 'therein', 'can', 'seemed', 'hereby', 'others', 
-        "'s", "'re", 'most', 'one', "n't", 'into', 'some', 'will', 'these', 'twenty', 'here', 'as', 'nobody', 
-        'also', 'along', 'than', 'anything', 'he', 'there', 'does', 'we', '’ll', 'latterly', 'are', 'ten', 
-        'hers', 'should', 'they', '‘s', 'either', 'am', 'be', 'perhaps', '’re', 'only', 'namely', 'sixty', 
-        'made', "'m", 'always', 'those', 'have', 'again', 'her', 'once', 'ours', 'herself', 'else', 'has', 'nine', 
-        'more', 'sometimes', 'your', 'yours', 'that', 'around', 'his', 'indeed', 'mostly', 'cannot', '‘ll', 'too', 
-        'seems', '’m', 'himself', 'latter', 'whither', 'amount', 'other', 'nevertheless', 'whom', 'for', 'somehow', 
-        'beforehand', 'just', 'an', 'beyond', 'amongst', 'none', "'ve", 'say', 'via', 'but', 'often', 're', 'our', 
-        'because', 'rather', 'using', 'without', 'throughout', 'on', 'she', 'never', 'eight', 'no', 'hereupon', 
-        'them', 'whereafter', 'quite', 'which', 'move', 'thru', 'until', 'afterwards', 'fifty', 'i', 'itself', 'n‘t',
-        'him', 'could', 'front', 'within', '‘re', 'back', 'such', 'already', 'several', 'side', 'whence', 'me', 
-        'same', 'were', 'it', 'every', 'third', 'together'
-    ]
-}
-
 
 ### START FUNCTION
 def word_splitter(df):
+    """
+    Split each tweet into individual words and store them in a new column.
+
+    Args:
+    df (DataFrame): DataFrame containing the 'Tweets' column.
+
+    Returns:
+    DataFrame: Modified DataFrame with a new column 'Split Tweets' containing lists of individual words.
+    """
     pd.set_option('display.max_columns', None) # Set to display all columns in output
 
-    twts_lst = list(df['Tweets'].values) # Converts 'Tweet 'column into a numpy array then into a list
-    #print(twts_lst)# Observing output
+    # Extract the tweets as a list of strings
+    twts_lst = list(df['Tweets'].values)
 
-    # Code takes a list of strings "twts_lst" and splits each string into a list of strings, storing all these new lists
-    # into a column in the "df" dataframe named "Split Tweets"
+    # Split each tweet into individual words, convert to lowercase, and store them in a new column
     df["Split Tweets"] =  [[word.lower() for word in twt_str.split()] for twt_str in twts_lst]
-    
-    #print(df["Split Tweets"]) # Observing output
 
-    return print(df) # NB>>> Remove print()
+    return df
+
 ### END FUNCTION
 
+# Input:
 word_splitter(twitter_df.copy())
+
+# Output:
+output_data = word_splitter(twitter_df.copy())
+print(output_data)
